7f0667e099b99af4c8483443966d43c9fab841a0
Reini Urban <reini.urban@gmail.com>
2025-12-23 13:16:01 +0100

    autotools compat GNUisms
    macos needs autoreconf -fi, which complains about GNUisms

--- lib/syck.h
+++ lib/syck.h
@@ -13,10 +13,12 @@
 #define SYCK_YAML_MAJOR 1
 #define SYCK_YAML_MINOR 0
 
-#define SYCK_VERSION    "0.61"
+#define SYCK_VERSION    "0.70"
 #define YAML_DOMAIN     "yaml.org,2002"
 
+#ifdef HAVE_CONFIG_H
 #include "config.h"
+#endif
 #include <stdlib.h>
 #include <stddef.h>
 #include <stdio.h>
